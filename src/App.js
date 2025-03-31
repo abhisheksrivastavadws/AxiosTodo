294748e0957d553c9d8e1bf458b52b6a19dbb9ac
abhisheksrivastavadws <43194404+abhisheksrivastavadws@users.noreply.github.com>
2025-03-31 21:59:01 +0530

    Please refer comments inside file App.js
    Please refer comments inside file App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Todos from "./Axios/Todos";
 import axios from "axios";
 
 export default function App() {
+  // Jtiy, Using Axios inside useState, seems good & useState is the first thing that runs before useEffect. But ideally useEffect is used for condition based rendering ( entry of new Data) or timer 
   const [todoTask, setTodos] = useState([
     axios
       .get("https://jsonplaceholder.typicode.com/todos")
@@ -21,7 +22,7 @@ export default function App() {
       })
     );
   };
-
+// what will happen if API fails with 504  error, 50x error , 404 error , 401 error , empty data. Error handling need to be done. 
   return (
     <div>
       <Header title="My Todos" />
